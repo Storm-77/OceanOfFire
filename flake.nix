84dd1e6424293eeb5a6ba557fc53646df5867189
Storm-77 <czechu600@gmail.com>
2024-12-18 20:09:30 +0100

    separate go install dir

--- flake.nix
+++ flake.nix
@@ -15,8 +15,13 @@
             pkgs.nodejs 
             pkgs.nodePackages.typescript
             pkgs.nodePackages.npm
+            pkgs.go
         ];
         shellHook = ''
+            export REAL_PATH=$(realpath .)
+            export GOBIN=$REAL_PATH/.go       # set path to install go binaries to
+            export PATH=$REAL_PATH/.go:$PATH  # shell path variable to access go binary
+        
         '';
 
 
